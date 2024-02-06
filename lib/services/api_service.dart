9c6f9841b2b1dbe62094b081b0fa7eb9b994e886
debrasnicolas <nicodebras17@gmail.com>
2024-02-06 22:46:46 +0100

    work

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,17 @@
 import 'package:dio/dio.dart';
-import 'package:xenflutter/models/user.dart';
-import 'package:xenflutter/models/post.dart';
-import 'package:xenflutter/services/signup_request.dart';
-import 'package:xenflutter/services/signup_response.dart';
 
 class ApiService {
-  final Dio _dio;
+   late Dio _dio;
 
-  ApiService({required String baseUrl}) : _dio = Dio(BaseOptions(baseUrl: baseUrl ?? 'https://xoc1-kd2t-7p9b.n7c.xano.io/api:xbcc5VEi'));
+   ApiService() {
+    _dio = Dio();
 
-  //à retirer potentiellement
-  Future<SignupResponse> signUp(SignupRequest request) async {
-    final response = await _dio.post('/auth/signup', data: request.toJson());
-    return SignupResponse.fromJson(response.data);
+    _dio.options.baseUrl = 'https://xoc1-kd2t-7p9b.n7c.xano.io/api:xbcc5VEi/';
+    _dio.options.headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    };
+
   }
+   Dio get dio => _dio;
 }

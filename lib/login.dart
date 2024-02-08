4720b9f84cb5203695c512515b42204e984c0ef8
Capdrake <bastienleuwers2@gmail.com>
2024-02-08 13:54:43 +0100

    add register page and modify login and test

---
+++ lib/login.dart
@@ -0,0 +1,71 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xenflutter/services/AuthService.dart';
+import 'package:xenflutter/services/api_service.dart';
+import 'package:xenflutter/services/request/login_request.dart';
+import 'package:xenflutter/services/response/login_response.dart';
+
+import 'models/AuthState.dart';
+
+class Login extends StatelessWidget {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final apiService = Provider.of<ApiService>(context, listen: false);
+    final AuthService authService = AuthService(apiService.dio);
+
+    return Expanded(
+      child: Container(
+        color: Colors.indigoAccent,
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email, color: Colors.white,),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Mot de passe',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock, color: Colors.white,),
+                ),
+                obscureText: true,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                String email = emailController.text;
+                String password = passwordController.text;
+                print(email);
+                print(password);
+                final requestJson = LoginRequest(email: email, password: password);
+                final signInResponse = await authService.signIn(requestJson);
+                final loginResponse = LoginResponse(user: signInResponse.user, authToken: signInResponse.authToken);
+
+                // Trouver AuthState dans le contexte et mettre à jour l'état
+                final authState = Provider.of<AuthState>(context, listen: false);
+                authState.login(loginResponse.authToken, loginResponse.user);
+              },
+              child: Text('Se connecter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/register.dart
+++ lib/register.dart
@@ -1,71 +1,87 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:xenflutter/services/request/signup_request.dart';
 import 'package:xenflutter/services/AuthService.dart';
-import 'package:xenflutter/services/api_service.dart';
-import 'package:xenflutter/services/request/login_request.dart';
-import 'package:xenflutter/services/response/login_response.dart';
-
-import 'models/AuthState.dart';
+import 'package:xenflutter/services/response/signup_response.dart';
 
 class Register extends StatelessWidget {
-
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final apiService = Provider.of<ApiService>(context, listen: false);
-    final AuthService authService = AuthService(apiService.dio);
+    final AuthService authService = Provider.of<AuthService>(context, listen: false);
 
     return Expanded(
       child: Container(
-        color: Colors.indigoAccent,
+        color: Colors.lightBlue,
         child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.email, color: Colors.white,),
-                ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: TextFormField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Mot de passe',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.lock, color: Colors.white,),
-                ),
-                obscureText: true,
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                String email = emailController.text;
-                String password = passwordController.text;
-                print(email);
-                print(password);
-                final requestJson = LoginRequest(email: email, password: password);
-                final signupResponse = await authService.signIn(requestJson);
-                final loginResponse = LoginResponse(user: signupResponse.user, authToken: signupResponse.authToken);
-
-                // Trouver AuthState dans le contexte et mettre à jour l'état
-                final authState = Provider.of<AuthState>(context, listen: false);
-                authState.login(loginResponse.authToken, loginResponse.user);
-              },
-              child: Text('Se connecter'),
-            ),
-          ],
+            children: [
+        //nom
+        Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: TextFormField(
+          controller: nameController,
+          decoration: const InputDecoration(
+            labelText: 'Nom',
+            border: OutlineInputBorder(),
+            prefixIcon: Icon(Icons.person, color: Colors.white,),
+          ),
         ),
       ),
+      //email
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: TextFormField(
+          controller: emailController,
+          decoration: const InputDecoration(
+            labelText: 'Email',
+            border: OutlineInputBorder(),
+            prefixIcon: Icon(Icons.email, color: Colors.white,),
+          ),
+          keyboardType: TextInputType.emailAddress,
+        ),
+      ),
+      //mot de passe
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: TextFormField(
+          controller: passwordController,
+          decoration: const InputDecoration(
+            labelText: 'Mot de passe',
+            border: OutlineInputBorder(),
+            prefixIcon: Icon(Icons.lock, color: Colors.white,),
+          ),
+          obscureText: true,
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () async {
+          String email = emailController.text;
+          String password = passwordController.text;
+          String name = nameController.text;
+          print(email); //pour debug
+          print(password); //idem
+          final requestJson = SignupRequest(name: name, email: email, password: password);
+          try {
+            final signupResponse = await authService.signUp(requestJson);
+            //ici la gestion post-inscription : retour page de co et message pour dire c'est ok
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Inscription réussie')),
+            );
+          } catch (e) {
+            //erreur d'inscription
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Erreur d\'inscription: $e')),
+            );
+          }
+        },
+        child: Text('S\'inscrire'),
+        ),
+        ],
+      ),
+    ),
     );
   }
 }

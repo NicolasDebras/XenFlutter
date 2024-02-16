b19e7675f11ac432fb9f5e395cc70b3ea3a637e1
debrasnicolas <nicodebras17@gmail.com>
2024-02-16 10:20:51 +0100

    last push, last

--- lib/design/Post/AddEditPostWidget.dart
+++ lib/design/Post/AddEditPostWidget.dart
@@ -6,6 +6,9 @@ import '../../services/PostsService.dart';
 import '../../services/provider/AuthState.dart';
 import '../../services/provider/PostsProvider.dart';
 import '../../services/provider/api_service.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'dart:io';
 
 class AddEditPostWidget extends StatefulWidget {
   final Post? post;
@@ -19,6 +22,7 @@ class AddEditPostWidget extends StatefulWidget {
 class _AddEditPostWidgetState extends State<AddEditPostWidget> {
   final _formKey = GlobalKey<FormState>();
   late TextEditingController _contentController;
+  File? _image;
 
   @override
   void initState() {
@@ -32,6 +36,17 @@ class _AddEditPostWidgetState extends State<AddEditPostWidget> {
     super.dispose();
   }
 
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
   void _addOrEditPost() async {
     if (_formKey.currentState!.validate()) {
       final content = _contentController.text;
@@ -41,46 +56,58 @@ class _AddEditPostWidgetState extends State<AddEditPostWidget> {
         final authState = Provider.of<AuthState>(context, listen: false);
         final postsProvider = Provider.of<PostsProvider>(context, listen: false);
 
+        Post post;
         if (widget.post == null) {
-          Post post = await postsService.addPost(content: content, token: authState.authToken);
+          post = await postsService.addPost(content: content, image: _image, token: authState.authToken);
           postsProvider.addPost(post);
         } else {
-          await postsService.editPost(postId: widget.post!.id!, content: content, token: authState.authToken);
+          await postsService.editPost(postId: widget.post!.id!, content: content, image: _image, token: authState.authToken);
           postsProvider.editPost(content, widget.post!.id!);
         }
         Navigator.of(context).pop();
       } catch (e) {
-        // Gérer l'erreur ici
-        print("Erreur lors de l'ajout/édition du post: $e");
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Erreur lors de l\'ajout/édition du post: $e')));
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextFormField(
-            controller: _contentController,
-            decoration: InputDecoration(labelText: 'Contenu'),
-            maxLines: null,
-            minLines: 3,
-            keyboardType: TextInputType.multiline,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Veuillez entrer du contenu';
-              }
-              return null;
-            },
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: _contentController,
+                decoration: InputDecoration(labelText: 'Contenu'),
+                maxLines: null,
+                minLines: 3,
+                keyboardType: TextInputType.multiline,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer du contenu';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              _image != null ? Image.file(_image!) : Container(),
+              TextButton(
+                onPressed: _pickImage,
+                child: Text('Choisir une image'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _addOrEditPost,
+                child: Text(widget.post == null ? 'Ajouter' : 'Éditer'),
+              ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: _addOrEditPost,
-            child: Text(widget.post == null ? 'Ajouter' : 'Éditer'),
-          ),
-        ],
+        ),
       ),
     );
   }
